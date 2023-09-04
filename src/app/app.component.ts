a7ddea5b1e92e92194a9d2822d61dc90cdb8b5bd
Tommy <tommycheny@163.com>
2023-09-04 23:06:11 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Component, OnInit } from '@angular/core';
+import { LoadingComponent } from './loading/loading.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mf-shell';
+
+  public currentComponent: any = LoadingComponent
+
+  async ngOnInit() {
+    const module = await  loadRemoteModule({
+      type:'module',
+      remoteEntry: 'http://localhost:4300/remoteEntry.js',
+      exposedModule: './Component'
+    })
+    this.currentComponent = module.AppComponent
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule
